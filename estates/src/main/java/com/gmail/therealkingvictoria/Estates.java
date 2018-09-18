fbbcad90be79b643e496d9fbdc6d11afb2ce3ab2
KingVictoria <TheRealKingVictoria@gmail.com>
2018-09-18 09:07:38 -0400

    Initial Commit

---
+++ estates/src/main/java/com/gmail/therealkingvictoria/Estates.java
@@ -0,0 +1,15 @@
+package com.gmail.therealkingvictoria;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Estates extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	} // onEnable
+	
+	public void onDisable() {
+		
+	} // onDisable
+
+} // class
