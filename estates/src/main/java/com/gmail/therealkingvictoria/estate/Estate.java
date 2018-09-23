fdadb0b91806070db52c78bc01040c9ddb257ede
KingVictoria <TheRealKingVictoria@gmail.com>
2018-09-23 10:57:51 -0400

    Removed Cost from Estate (since Estates are not produced at Estates)

--- estates/src/main/java/com/gmail/therealkingvictoria/estate/Estate.java
+++ estates/src/main/java/com/gmail/therealkingvictoria/estate/Estate.java
@@ -1,15 +1,22 @@
 package com.gmail.therealkingvictoria.estate;
 
 import org.bukkit.Location;
+import org.bukkit.event.block.BlockPlaceEvent;
 
-import com.gmail.therealkingvictoria.module.Cost;
 import com.gmail.therealkingvictoria.multiblock.MultiBlock;
+import com.gmail.therealkingvictoria.multiblock.Structure;
 
 public class Estate {
 	
-	String name;
-	Location location;
-	MultiBlock structure;
-	Cost cost;
+	private String name;
+	private Location location;
+	private Structure structure;
+	
+	Estate(String name, Location location, MultiBlock multiBlock, BlockPlaceEvent event) {
+		this.name = name;
+		this.location = location;
+
+		structure = new Structure(multiBlock, location, event);
+	} // Estate
 
 } // class
