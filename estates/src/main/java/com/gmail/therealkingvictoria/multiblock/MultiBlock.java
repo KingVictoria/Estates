88c0b054eec8f54fa87fc5f9fab1983003f86984
KingVictoria <TheRealKingVictoria@gmail.com>
2018-09-23 10:56:49 -0400

    Basics of MultiBlock/Structure system laid out

--- estates/src/main/java/com/gmail/therealkingvictoria/multiblock/MultiBlock.java
+++ estates/src/main/java/com/gmail/therealkingvictoria/multiblock/MultiBlock.java
@@ -1,5 +1,40 @@
 package com.gmail.therealkingvictoria.multiblock;
 
-public class MultiBlock {
+import java.util.List;
 
-}
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * A blueprint for a Structure
+ * @author KingVictoria
+ */
+public class MultiBlock {
+	
+	private List<Structure> structures;
+	private ItemStack base;
+	
+	/**
+	 * Adds a reference to a physical Structure of this MultiBlock to the list
+	 * @param structure Structure to add
+	 */
+	public void addStructure(Structure structure) { 
+		structures.add(structure);
+	} // addStructure
+	
+	/**
+	 * Removes a reference to a physical Structure of this MultiBlock from the list
+	 * @param structure Structure to remove
+	 */
+	public void removeStructure(Structure structure) {
+		structures.remove(structure);
+	} // removeStructure
+	
+	/**
+	 * Gets the base block of the MultiBlock as an ItemStack
+	 * @return ItemStack base block
+	 */
+	public ItemStack getBase() { 
+		return base; 
+	} // getBase
+
+} // class
---
+++ estates/src/main/java/com/gmail/therealkingvictoria/multiblock/Structure.java
@@ -0,0 +1,78 @@
+package com.gmail.therealkingvictoria.multiblock;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * A specific MultiBlock Structure that exists in the world representing an Estate or Module
+ * @author KingVictoria
+ */
+public class Structure {
+	
+	private MultiBlock multiBlock;
+	private Location location;
+	private int health;
+	
+	/**
+	 * Creates a Structure of a MultiBlock, a physical instance of a type of MultiBlock
+	 * @param multiBlock the type of MultiBlock this structure is
+	 * @param location Location of MultiBlock
+	 * @param event BlockPlaceEvent that created this structure (all structures are made by placing a base block)
+	 */
+	public Structure(MultiBlock multiBlock, Location location, BlockPlaceEvent event) {
+		this.multiBlock = multiBlock;
+		this.location = location;
+		
+		if(!build()) {
+			event.setCancelled(true);
+			return;
+		} // if
+		
+		multiBlock.addStructure(this);
+	} // Structure
+
+	/** Unimplemented */
+	public boolean build() { return false; } // build
+	/** Unimplemented */
+	public boolean destroy() { return false; } // destroy
+	/** Unimplemented */
+	public boolean destroy(Player player) { return false; } // destroy(Player)
+	/** Unimplemented */
+	public Location partOf(Block block) { return null; } // partOf
+	
+	/**
+	 * Sets the health of this structure (when health reaches zero it is destroyed)
+	 * @param health int number to set health to
+	 */
+	public void setHealth(int health) { 
+		this.health = health; 
+	} // setHealth
+	
+	/**
+	 * Gets the health of this structure (when health reaches zero it is destroyed)
+	 * @return int health
+	 */
+	public int getHealth() { 
+		return health; 
+	} // getHealth
+	
+	/**
+	 * Gets the Location of this Structure
+	 * @return Location of structure
+	 */
+	public Location getLocation() { 
+		return location; 
+	} // getLocation
+	
+	/**
+	 * Gets the base block of this Structure's MultiBlock as an ItemStack
+	 * @return ItemStack base block
+	 */
+	public ItemStack getBase() {
+		return multiBlock.getBase();
+	} // getBase
+
+} // class
